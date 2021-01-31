777257d0ad657835d5801f64be9b8434ca829a81
Alexandre Oliveira <alexandreoliveira@aleoliv.dev>
2021-01-31 18:32:40 -0300

    USE CASE: Create usa case for Cards

---
+++ Src/UseCases/Cards/Create/CardsCreateResponseDto.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PlanningPokerApi.Src.UseCases.Cards.Create
+{
+  public class CardsCreateResponseDto
+  {
+
+    public Guid Id { get; set; }
+
+    public int Value { get; set; }
+  }
+}
