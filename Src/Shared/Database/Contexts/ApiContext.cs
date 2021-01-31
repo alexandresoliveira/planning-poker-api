99ed387d808e570dd017298a4245d171c13f7daa
Alexandre Oliveira <alexandreoliveira@aleoliv.dev>
2021-01-31 17:05:02 -0300

    Overriding save change methods for update dates

--- Src/Shared/Database/Contexts/ApiContext.cs
+++ Src/Shared/Database/Contexts/ApiContext.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using PlanningPokerApi.Src.Shared.Database.Entities;
 
@@ -5,7 +8,6 @@ namespace PlanningPokerApi.Src.Shared.Database.Contexts
 {
   public class ApiContext : DbContext
   {
-
     public DbSet<UserEntity> Users { get; set; }
 
     public DbSet<CardEntity> Cards { get; set; }
@@ -16,5 +18,53 @@ namespace PlanningPokerApi.Src.Shared.Database.Contexts
 
     public ApiContext(DbContextOptions<ApiContext> options) : base(options)
     { }
+
+    public override int SaveChanges(bool acceptAllChangesOnSuccess)
+    {
+      OnBeforeSaving();
+      return base.SaveChanges(acceptAllChangesOnSuccess);
+    }
+
+    public override async Task<int> SaveChangesAsync(
+      bool acceptAllChangesOnSuccess,
+      CancellationToken cancellationToken = default(CancellationToken))
+    {
+      OnBeforeSaving();
+      return (await base.SaveChangesAsync(acceptAllChangesOnSuccess,
+                    cancellationToken));
+    }
+
+    private void OnBeforeSaving()
+    {
+      var entries = ChangeTracker.Entries();
+      var utcNow = DateTime.UtcNow;
+
+      foreach (var entry in entries)
+      {
+        // for entities that inherit from BaseEntity,
+        // set UpdatedOn / CreatedOn appropriately
+        if (entry.Entity is BaseEntity trackable)
+        {
+          switch (entry.State)
+          {
+            case EntityState.Modified:
+              // set the updated date to "now"
+              trackable.UpdatedAt = utcNow;
+
+              // mark property as "don't touch"
+              // we don't want to update on a Modify operation
+              entry.Property("CreatedOn").IsModified = false;
+              break;
+
+            case EntityState.Added:
+              // set both updated and created date to "now"
+              trackable.CreatedAt = utcNow;
+              trackable.UpdatedAt = utcNow;
+              break;
+          }
+        }
+      }
+    }
+
   }
 }
