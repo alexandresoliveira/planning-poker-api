6c99a7fcf4105ffac1135a8f3e3597a76c4d8cba
Alexandre Oliveira <alexandreoliveira@aleoliv.dev>
2021-01-31 16:54:49 -0300

    Project files

---
+++ Startup.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using PlanningPokerApi.Src.Shared.Database.Contexts;
+using PlanningPokerApi.Src.Shared.Database.Repositories;
+
+namespace PlanningPokerApi
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<ApiContext>(opt => opt.UseNpgsql(Configuration.GetConnectionString("PlanningPokerApiConnectionUrl")));
+      services.AddScoped<ApiContext, ApiContext>();
+      // services.Add(new ServiceDescriptor(typeof(IRepository<T extends BaseEntity>), new UserRepository()));
+      services.AddControllers();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
+}
