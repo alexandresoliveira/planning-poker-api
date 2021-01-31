bd88a57d0ed20d2e14bff3e408597530bbb96baf
Alexandre Oliveira <alexandreoliveira@aleoliv.dev>
2021-01-31 17:56:36 -0300

    Add required validation on attribute Name

--- Src/Shared/Database/Entities/UserEntity.cs
+++ Src/Shared/Database/Entities/UserEntity.cs
@@ -9,6 +9,7 @@ namespace PlanningPokerApi.Src.Shared.Database.Entities
   {
     [MaxLength(50, ErrorMessage = "Max value of {0} is {1}")]
     [MinLength(3, ErrorMessage = "Min value of {0} is {1}")]
+    [Required(ErrorMessage = "{0} collumn is required")]
     [Column("name")]
     public string Name { get; set; }
   }
