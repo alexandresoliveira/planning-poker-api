ec5099490354d383f3e7eba69c8a1dbc098ad09d
Alexandre Oliveira <alexandreoliveira@aleoliv.dev>
2021-02-02 13:35:04 -0300

    Authentication injects

--- Src/Shared/Injects/AuthenticationInject.cs
+++ Src/Shared/Injects/AuthenticationInject.cs
@@ -1,8 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
-using PlanningPokerApi.Src.Shared.Database.Repositories;
-using PlanningPokerApi.Src.Shared.Database.Entities;
 using PlanningPokerApi.Src.Shared.Business;
-using PlanningPokerApi.Src.UseCases.V1.Authentication;
+using PlanningPokerApi.Src.UseCases.V1.Authentication.Login;
 
 namespace PlanningPokerApi.Src.Shared.Injects
 {
@@ -10,7 +8,7 @@ namespace PlanningPokerApi.Src.Shared.Injects
   {
     public void Invoke(IServiceCollection services)
     {
-      services.AddScoped<IBusiness<AuthenticationRequestDto, AuthenticationResponseDto>, AuthenticationBusiness>();
+      services.AddScoped<IBusiness<AuthenticationLoginRequestDto, AuthenticationLoginResponseDto>, AuthenticationLoginBusiness>();
     }
   }
 }
